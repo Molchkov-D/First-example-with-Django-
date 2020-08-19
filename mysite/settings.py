616f1b9c0443e82b3988d4fde1445509d97fdd11
Molchkov Daniil <cakeof.m@mail.ru>
2020-08-19 11:17:42 +0300

    1.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pzb5&43u5+_m!j2w_56cjfoj@w*3jse4@1tv2@g_jt_6_obpes'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['keks.com1']
 
 
 # Application definition
